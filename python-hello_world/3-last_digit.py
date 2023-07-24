de90802de9049727031b694bfa8c8bfadc0be260
Regomoditsoe <rsmramarumo@gmail.com>
2023-07-24 11:48:22 -0700

    The last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,12 +2,14 @@
 import random
 number = random.randint(-10000, 10000)
 
-The_Last_digit = abs(number) % 10
-print('The last string digit of', number, 'is', The_Last_digit, end=" ")
+number_str = str(number)
+Last_digit = int(number_str[-1])
 
-if The_Last_digit > 5:
+print("The last digit of", number, "is", Last_digit, end=" ")
+
+if Last_digit > 5:
     print('and is greater than 5');
-elif The_Last_digit == 0:
+elif Last_digit == 0:
     print('and is 0');
 else:
     print('and is less than 6 and not 0');
