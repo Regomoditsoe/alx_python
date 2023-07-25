ef662d1efe9489c59bfc6287223d0c60fb4de4f1
Regomoditsoe <rsmramarumo@gmail.com>
2023-07-25 14:09:47 -0700

    00...99

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,4 @@
 for number in range(10):
     for object in range(number+1, 10):
-        print("{:02d} ".format(number*10+object), end=", "  if number *10 + object < 89 else "")
+        print("{:02d}".format(number*10+object), end=", "  if number *10 + object < 89 else "")
 print()
