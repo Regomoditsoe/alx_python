cb8fc1a59aad4fbb3b3c6ba36b7cafccdb1ebbe7
Regomoditsoe <rsmramarumo@gmail.com>
2023-07-31 08:22:24 -0700

    a + b

---
+++ 0-sum.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def add(a, b):
+    return a + b
+print(add(1, 2))
+
+def add(c, d):
+    return c + d
+print(add(98, 0))
+
+def add(e, f):
+    return e + f
+print(add(100, -2))
