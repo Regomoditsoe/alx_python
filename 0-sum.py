8b6ad093a2342a705b28da7783fe4a4d6078d2a4
Regomoditsoe <rsmramarumo@gmail.com>
2023-07-31 08:38:55 -0700

    a + b

--- 0-sum.py
+++ 0-sum.py
@@ -2,16 +2,15 @@
 
 def add(a, b):
     return a + b
-print(add(1, 2))
 
-def add(c, d):
-    return c + d
-print(add(100, -2))
+result1 = add(1, 2)
+print(result1)
 
-def add(e, f):
-    return e + f
-print(add(-100, -2))
+result2 = add(100, -2)
+print(result2)
 
-def add(g, h):
-    return g + h
-print(add(0, 0))
+result3 = add(-100, -2)
+print(result3)
+
+result4 = add(0, 0)
+print(result4)
