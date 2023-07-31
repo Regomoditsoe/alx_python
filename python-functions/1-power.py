cdca64cb3d3956dafce8635c08de80290838207a
Regomoditsoe <rsmramarumo@gmail.com>
2023-07-31 14:42:57 -0700

    a ^ b

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,8 +1,19 @@
 def pow(a, b):
-    a ^ b;
+    result = 1
+    for _ in range(b):
+        result *= a
+    return result
+result_1 = pow(2, 2)
+print(result_1)
 
-    print(pow(2, 2))
-    print(pow(98, 2))
-    print(pow(98, 0))
-    print(pow(100, -2))
-    print(pow(-4, 5))
+result_2 = pow(98, 2)
+print(result_2)
+
+result_3 = pow(98,0)
+print(result_3)
+
+result_4 = pow(100, -2)
+print(result_4)
+
+result_5 = pow(-4, 5)
+print(result_5)
