4c21ae9726e142d780bfcff971f9a7df544934d1
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-02 06:04:37 -0700

    Convert temp to celsius

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def convert_to_celsius(fahrenheit):
-    """Converts temp from fahrenheit to celsius"""
-    celsius = (fahrenheit - 32) * 5/9
-    return celcious
+    celsius = (fahrenheit - 32) * 5 / 9
+    return celsius
