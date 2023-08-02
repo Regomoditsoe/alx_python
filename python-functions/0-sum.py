ba9a23a9436a6300517abafed1377be5a30c80da
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-02 05:29:42 -0700

    a + b

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,14 +1,3 @@
 def add(a, b):
+    """Return addition of a and b."""
     return (a + b)
-
-result_1 = add(1, 2)
-print(result_1)
-
-result_2 = add(-100, -2)
-print(result_2)
-
-result_3 = add(100, -2)
-print(result_3)
-
-result_4 = add(0, 0)
-print(result_4)
