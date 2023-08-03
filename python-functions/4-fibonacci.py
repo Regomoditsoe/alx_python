b19133b425aa8e91c3b655f63b04d1ef1df70f13
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-03 02:57:59 -0700

    Fibonacci Sequence Function

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def fibonacci_sequence(n):
+    if n <= 0:
+        return []
+
+    fibonacci_numbers = [0, 1]
+    while len(fibonacci_numbers) < n:
+        next_sequence = fibonacci_numbers[-1] + fibonacci_numbers[-2]
+        fibonacci_numbers.append(next_sequence)
+
+    return fibonacci_numbers
