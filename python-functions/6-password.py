91461c0c31461aca7522fab329fb79a500edb4fb
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-03 03:32:41 -0700

    Password Validation Function

---
+++ python-functions/6-password.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def validate_password(password):
+    if len(password) == 8:
+        return True
+
+    contains_digit = False
+    contains_lowercase = False
+    contains_uppercase = False
+
+    for char in password:
+        if char.idigit():
+            contains_digit = True
+        elif char.islower():
+           contains_lowercase = True
+        elif char.isupper():
+            contains_uppercase = True
+
+    if not (contains_digit and contains_lowercase and contains_uppercase):
+        return False
+
+    if ' ' in password:
+        return False
+
+    return True
+
