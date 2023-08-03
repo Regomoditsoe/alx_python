d5c17fd666b7c48e654dd7ace53b8519e3e7c38b
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-03 03:11:24 -0700

    Prime number?

---
+++ python-functions/5-prime.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def is_prime(number):
+    if number < 2:
+        return False
+
+    for n in range(2, int(number ** 0.5) + 1):
+        if number % n == 0:
+            return False
+
+    return True
