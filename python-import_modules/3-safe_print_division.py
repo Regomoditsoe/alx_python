fb30be61f1d5213c14e2632bb377ffee9da5028c
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-04 10:27:55 -0700

    Integers division with debug

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -9,5 +9,6 @@ def safe_print_division(a, b):
         print("Inside result: {}".format(result))
         if result is not None:
             print("{} / {} = {}".format(a, b, result))
-            return result
+    return result
+        
 
