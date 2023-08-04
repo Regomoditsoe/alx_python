ec9369de414d143e827083b69f1a83c8f124125c
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-04 04:44:19 -0700

    Integers division with debug

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -6,7 +6,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result = None
     finally:
-        print("Inside result: {}".format(result))
-        if result is not None:
-            print("{} / {} = {}".format(a, b, result))
-            return result
+        print("{} / {} = {}".format(a, b, result))
+    return result
