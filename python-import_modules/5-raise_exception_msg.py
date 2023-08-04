b0ebddde38cfbdf78fc57d373fc49c0bf5297180
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-04 06:14:35 -0700

    Raise exception

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
 def raise_exception_msg(message=""):
-    if not message:
-        message = ""
-    raise NameError(message)
-try:
+    try:
     raise_exception_msg("")
 except NameError as ne:
     print(ne)
