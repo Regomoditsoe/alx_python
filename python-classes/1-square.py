f57488105c898fc4d147872665989bb85f8e3526
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-08 13:22:18 -0700

    Size validation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 
-def __init__(self, size=0):
+class Square:
+    """Represents the square"""
+
+    def __init__(self,size=0):
+        """Initialize new square
+        Args:
+        size(int): The size of a new square"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = 0
