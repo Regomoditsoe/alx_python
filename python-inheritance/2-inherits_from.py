b2b94903018339c88ad8922e538260d11c48505e
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-11 05:36:43 -0700

    Only sub class of

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+"""Define an inheretance"""
 def inherits_from(obj, a_class):
+    """Check if the object is an instance of a class inherited
+    Args:
+        obj: object to be checked.
+        a_class: the class to match the object to.
+    Return: True - if the object is an instance of class.
+        False - Otherwise."""
+
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    else:
+        True
