9bc1793316f4f56e30783bb50336b711814dbc02
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-11 05:21:48 -0700

    Same class or inherit from

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
-
+"""Defines a class of a function"""
 def is_kind_of_class(obj, a_class):
+    """Check if  the object is an instance or of a class
+    Args:
+        obj: object to be dependant on
+        a_class: the class needed to match the object
+    Returns: True  if the object is of an instance.
+        False:  Otherwise."""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
