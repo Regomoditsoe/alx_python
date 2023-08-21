779f6d80ea892ba73f8849886e91b809c1a6e05b
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-21 07:19:34 -0700

    Get all states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,29 @@
 #!user/bin/python3
-"""List all the states with a name starting with N from database hbtn_0e_0_usa."""
+"""List all states with a name starting with N from database hbtn_0e_0_usa."""
 import sys
 import MySQLdb
 
 
 if __name__ == "__main__":
     # Get MySQL credentials from command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
     # Connect to MySQL server
     db = MySQLdb.connect(
-            user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3])
+            user=username,
+            passwd=password,
+            db=database)
     c = db.cursor()
 
     # Execute the SQL query to retrieve all states sorted by id
-    c.execute("SELECT * FROM  `states` ORDER BY `id`")
-    [print(states) for state in c.fetchall() if state[1][0] == "N"]
+    c.execute("SELECT * FROM `states` WHERE `name` LIKE 'N%  ORDER BY `id`")
+
+    # Fetch and print results
+    states = c.fetchall()
+    for state in states:
+        print(state)
+
+    # Close database
+    db.close
