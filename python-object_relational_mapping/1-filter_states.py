bb6c82285eaf46887ff2255363d730b9b39a3d6b
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-21 07:05:36 -0700

    Filter states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!user/bin/python3
+"""List all the states with a name starting with N(upper N) from the database hbtn_0e_0_usa."""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    # Get MySQL credentials from command-line arguments
+    #Connect to MySQL server
+    db = MySQLdb.connect(
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3])
+    c = db.cursor ()
+
+    # Execute the SQL query to retrieve all states sorted by id
+    c.execute("SELECT * FROM  `states` ORDER BY `id`")
+    [print(states) for state in c.fetchall() if state[1][0] == "N"]
