3be163fb88f43211a00f3d0cfcaadafac5b38d8d
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-22 06:30:53 -0700

    Filter states

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -5,6 +5,7 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
+
     # Get MySQL credentials and search name from command line arguments
     # Connect to MySQL server
     db = MySQLdb.connect(
@@ -13,9 +14,9 @@ if __name__ == "__main__":
             db=sys.argv[3])
     c = db.cursor()
 
-    # Execute the SQL query to retrieve states with specified name
+    # Execute the SQL query to retrieve states with the specified name
     c.execute("SELECT * \
-                FROM `states` \
+                 FROM `states` \
                 WHERE BINARY `name` = '()'".format(sys.argv[4]))
 
     # Fetch all rows and print the states
