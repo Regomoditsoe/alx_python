8e9253fc9df93377f119e64ffd32fb1fdb1df771
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-22 05:58:25 -0700

    Filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -6,19 +6,16 @@ import MySQLdb
 
 if __name__ == "__main__":
     # Get MySQL credentials from command-line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
 
     # Connect to MySQL server
     db = MySQLdb.connect(
-            user=username,
-            passwd=password,
-            db=database)
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
     c = db.cursor()
 
     # Execute the SQL query to retrieve all states sorted by id
-    c.execute("SELECT * FROM `states` WHERE `name` LIKE 'N%  ORDER BY `id`")
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
 
     # Fetch and print results
     states = c.fetchall()
