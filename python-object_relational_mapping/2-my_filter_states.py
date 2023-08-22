f49e777430e3ab252c093cd1c18e1b8e26b6ecac
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-22 06:28:06 -0700

    Filter states

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/user/bin/python3
+"""Display all values in the states table of hbtn_0e_0_usa"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    # Get MySQL credentials and search name from command line arguments
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3])
+    c = db.cursor()
+
+    # Execute the SQL query to retrieve states with specified name
+    c.execute("SELECT * \
+                FROM `states` \
+                WHERE BINARY `name` = '()'".format(sys.argv[4]))
+
+    # Fetch all rows and print the states
+    [print(state) for state in c.fetchall()]
+
+    # Close database
+    db.close()
