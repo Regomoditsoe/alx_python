dabd13eea6be3b6eeb0f3080df9d0359fff8f948
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-23 09:55:25 -0700

    Filter state

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -22,11 +22,10 @@ if __name__ == "__main__":
     c = db.cursor()
 
     # Execute the SQL query to retrieve states with the specified name
-    query(
-            "SELECT * FROM states
+    query = "SELECT * FROM states
             WHERE name = '{}'
-            ORDER BY id ASC ".format(state_name)
-            )
+            ORDER BY id ASC".format(state_name)
+            
     c.execute(query)
 
     # Fetch all rows and print the states
