036aeaabe2285f6b1916842380d3c189ab354d3c
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-23 07:56:28 -0700

    My safe filter states

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,35 @@
+#!/user/bin/python3
+"""Display values in the states table of hbtn_0e_0_usa"""
+
+import sys
+import MySQL
+
+if __name == "__main__":
+
+
+    # Get the command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv [4]
+
+    # Connect to MySQL server
+    db = MySQL.connect(
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name,
+            host='localhost',
+            port=3306)
+    c = db.cursor()
+
+    # Prepare SQL query
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    c.execute(query, (state_name,))
+
+    rows = c.fetchall()
+
+    # Display results
+    for row in rows:
+        print(row)
+
+    db.close()
