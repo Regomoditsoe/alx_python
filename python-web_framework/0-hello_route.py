d2c8381dccf2e3b1eccfaa5c2f193fbf8dc0a4de
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-30 11:33:04 -0700

    Hello Flask!

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 @app.route ('/', strict_slashes=False)
 def hello():
     """Defining a route that returns the message Hello HBNB"""
-    return Hello HBNB
+    name = request.args.get("name", "World")
+    return "Hello HBNB"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
