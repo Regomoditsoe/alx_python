65c136266b090b3804e7ece46082e7db162061cf
Regomoditsoe <rsmramarumo@gmail.com>
2023-08-31 14:40:40 -0700

    Is it a number?

---
+++ python-web_framework/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Start a flask web application"""
+# save this as app.py
+
+from flask import Flask, request
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route ('/', strict_slashes=False)
+def hello():
+    """Defining a route that returns the message Hello HBNB!"""
+    name = request.args.get("name", "HBNB")
+    return f"Hello {escape(name)}!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Defining the route returning the message HBNB"""
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Defining a C route followed by value of text"""
+    text = text.replace("_", " ")
+    return f"C {escape(text)}"
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    """Defining a Python text followed by value of text"""
+    text = text.replace("_", " ")
+    return f"Python {escape(text)}"
+
+@app.route('/number/,int:n>', strict_slashes=False)
+def a_number(n):
+    """Defining a route if n is an integer"""
+    return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
