e69f49a9dcb1436ed0f0e2d9cdc6d9c8c59e59e9
Regomoditsoe <rsmramarumo@gmail.com>
2023-10-03 13:39:24 -0700

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,18 +6,11 @@ import requests
 import sys
 
 
-if __name__ == "__main__":
+def get_todo_list_of_employees(user_id):
+    """ Return employee names and to do list"""
     base_url = "https://jsonplaceholder.typicode.com/"
-    
-    if len(sys.argv) != 2:
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
-
-
-    employee_id = sys.argv[1]
-
-    user_url = f"{base_url}users/{employee_id}"
-    todos_url = f"{base_url}todos?userId={employee_id}"
+    user_url = f"{base_url}users/{user_id}"
+    todos_url = f"{base_url}todos?userId={user_id}"
 
     try:
         response_user = requests.get(user_url)
@@ -40,3 +33,13 @@ if __name__ == "__main__":
         print("Employee not found.")
     except Exception as e:
         print(f"An unexpected error occured: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 0-gather_data_from_an_API.py <user_id>")
+    else:
+        try:
+            user_id = int(sys.argv[1])
+            get_todo_list_of_employees(user_id)
+        except ValueError:
+            print("User ID must be an integer")
