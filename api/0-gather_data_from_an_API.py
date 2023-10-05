8298c6f01548cd69b75799e8252fee32f0f4f2d4
Regomoditsoe <rsmramarumo@gmail.com>
2023-10-05 05:24:36 -0700

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,30 +10,33 @@ def get_todo_list_of_employees(user_id):
     user_url = f"{base_url}users/{user_id}"
     todos_url = f"{base_url}todos?userId={user_id}"
 
-    try:
-        response_user = requests.get(user_url)
-        user_data = response_user.json()
+    response_user = requests.get(user_url)
+    user_data = response_user.json()
         
-        response_todos = requests.get(todos_url)
-        todos_data = response_todos.json()
+    response_todos = requests.get(todos_url)
+    todos_data = response_todos.json()
 
-        DONE_TASKS =[task for task in todos_data if task["completed"]]
+    DONE_TASKS =[task for task in todos_data if task["completed"]]
         
-        TOTAL_NUMBER_OF_TASKS = len(todos_data)
-        NUMBER_OF_DONE_TASKS = len(DONE_TASKS)
-        EMPLOYEE_NAME = user_data['name']
+    TOTAL_NUMBER_OF_TASKS = len(todos_data)
+    NUMBER_OF_DONE_TASKS = len(DONE_TASKS)
+    EMPLOYEE_NAME = user_data['name']
 
 
-        print(f"Employee {EMPLOYEE_NAME} is done with tasks ({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks ({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
 
-        for task in DONE_TASKS:
-            print(f"\t{task['title']}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occured: {e}")
-    except ValueError as ve:
-        print(f"Error parsing JSON response: {ve}")
+    for task in DONE_TASKS:
+        print(f"\t{task['title']}")
 
 if __name__ == "__main__":
-    user_id = int(input("Enter User ID: "))
+    if len(sys.argv) != 2:
+        print("Usage: python3 0-gather_data_from_an_API.py <user_id>")
+        sys.exit(1)
+
+    try:
+        user_id = int(sys.argv[1])
+    except ValueError:
+        print("User ID must be an integer")
+        sys.exit(1)
+
     get_todo_list_of_employees(user_id)
