c9e5fe8362687828e3804b433d0c5c8c83ff8fbc
Regomoditsoe <rsmramarumo@gmail.com>
2023-10-07 04:25:07 -0700

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,39 +4,42 @@ import requests
 import sys
 
 
-def get_todo_list_of_employees(user_id):
-    """ Return employee names and to do list"""
-    base_url = "https://jsonplaceholder.typicode.com/"
-    user_url = f"{base_url}users/{user_id}"
-    todos_url = f"{base_url}todos?userId={user_id}"
+def get_todo_list_of_employees(employee_id):
+    # define url
+    employee_url = "https://jsonplaceholder.typicode.com"
 
-    response_user = requests.get(user_url)
-    user_data = response_user.json()
-        
-    response_todos = requests.get(todos_url)
+    # A request to get employee details
+    response_employee = requests.get(f"{employee_url}/users/{employee_id}")
+    employee_data = response_employee.json()
+    EMPLOYEE_NAME = employee_data.get("name")
+
+    # Make requests for the todos 
+    response_todos = requests.get(f"{employee_url}/users/{employee_id}/todos")
     todos_data = response_todos.json()
 
-    DONE_TASKS =[task for task in todos_data if task["completed"]]
+    # Calculating number of done tasks
+    DONE_TASKS =[todo for todo in todos_data if todo["completed"]]
         
     TOTAL_NUMBER_OF_TASKS = len(todos_data)
     NUMBER_OF_DONE_TASKS = len(DONE_TASKS)
-    EMPLOYEE_NAME = user_data['name']
+    
 
 
-    print(f"Employee EMPLOYEE_NAME is done with tasks (NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):")
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks ({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
 
     for task in DONE_TASKS:
         print(f"\t{task['title']}")
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 0-gather_data_from_an_API.py <user_id>")
+        print("Usage: python gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
 
     try:
-        user_id = int(sys.argv[1])
+        employee_id = int(sys.argv[1])
     except ValueError:
-        print("User ID must be an integer")
+        print("Employee ID must be an integer.")
         sys.exit(1)
 
-    get_todo_list_of_employees(user_id)
+    get_todo_list_of_employees(employee_id)
