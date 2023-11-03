df90e655e136c841648401396c02046b3c3cc9f5
Regomoditsoe <rsmramarumo@gmail.com>
2023-11-03 12:38:06 -0700

    geometry module

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,5 +1,5 @@
 #!/usr/bin/pythoni3
-"""A Module for a class that represents the base geometry."""
+"""A Module for a class that represents the BaseGeometry"""
 
 class BaseGeometry:
     """Empty BaseGeometry class."""
